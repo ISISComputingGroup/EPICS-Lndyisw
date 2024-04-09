4646a711c75233f89868729b357458a204317001
vux62295 <vux62295@ndw2633.isis.cclrc.ac.uk>
2024-04-09 16:34:41 +0100

    Added IOC test cases

--- system_tests/responder.bat
+++ system_tests/responder.bat
@@ -1,2 +1,1 @@
-call ..\..\..\..\..\Python3\Scripts\snmpsim-command-responder --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=.\tests 
-exit
+start "The SNMP Emulator" ..\..\..\..\..\Python3\Scripts\snmpsim-command-responder --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=.\tests 
