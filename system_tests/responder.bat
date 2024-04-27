0e2932942ce5810d684ac3249554fb2c998f904f
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-04-28 00:05:50 +0100

    Avoid use of exe as it embeds python path

--- system_tests/responder.bat
+++ system_tests/responder.bat
@@ -1,3 +1,5 @@
 setlocal
 call %~dp0..\..\..\..\config_env.bat
-start "The SNMP Emulator" %PYTHON3DIR%\Scripts\snmpsim-command-responder.exe --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=%~dp0tests
+REM we cannot call use Scripts\snmpsim-command-responder.exe as it embeds path to build server within it when
+REM generated and so fails on system tests server 
+start "The SNMP Emulator" %PYTHON3% %~dp0responder.py --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=%~dp0tests
