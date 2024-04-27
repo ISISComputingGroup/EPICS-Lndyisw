97bacb5bd8d378397b0428c25b2662980fcdaa7a
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-04-27 15:57:08 +0100

    Tidy up error code return

--- system_tests/run_tests.bat
+++ system_tests/run_tests.bat
@@ -9,6 +9,11 @@ set "PYTHONUNBUFFERED=1"
 
 REM Command line arguments always passed to the test script
 SET ARGS=--test_and_emulator %~dp0
-call %PYTHON3% "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" %ARGS% %*
-taskkill /IM snmpsim-command-responder.exe /F 
-IF %ERRORLEVEL% NEQ 0 EXIT /b %errorlevel%
+%PYTHON3% -u "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" %ARGS% %*
+
+REM preserve errorlevel so not overwritten by taskkill
+set errcode=%errorlevel%
+
+taskkill /IM snmpsim-command-responder.exe /F >NUL 2>&1
+
+IF %errcode% NEQ 0 EXIT /b %errcode%
