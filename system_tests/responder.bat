f3aea47d4024beb3b6db2a4e8349c900facea86d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-04-27 22:38:58 +0100

    Use config_env for python path

--- system_tests/responder.bat
+++ system_tests/responder.bat
@@ -1,1 +1,3 @@
-start "The SNMP Emulator" %~dp0..\..\..\..\..\Python3\Scripts\snmpsim-command-responder.exe --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=%~dp0tests
+setlocal
+call %~dp0..\..\..\..\config_env.bat
+start "The SNMP Emulator" %PYTHON3DIR%\Scripts\snmpsim-command-responder.exe --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=%~dp0tests
