59a6492e2934b3d14df7482feb6bd78efd5f662e
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-05-02 22:58:45 +0100

    Remove start so get emulator log files

--- system_tests/responder.bat
+++ system_tests/responder.bat
@@ -2,4 +2,4 @@ setlocal
 call %~dp0..\..\..\..\config_env.bat
 REM we cannot call use Scripts\snmpsim-command-responder.exe as it embeds path to build server within it when
 REM generated and so fails on system tests server 
-start "The SNMP Emulator" %PYTHON3% %~dp0responder.py --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=%~dp0tests
+%PYTHON3% -u %~dp0responder.py --agent-udpv4-endpoint=127.0.0.1:161 --data-dir=%~dp0tests
