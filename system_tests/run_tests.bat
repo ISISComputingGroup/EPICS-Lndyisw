46a7892d8a51865f28f123fc3d257533db54eeee
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-05-11 20:14:08 +0100

    Set HOMEPATH if not present

--- system_tests/run_tests.bat
+++ system_tests/run_tests.bat
@@ -8,6 +8,11 @@ call "%~dp0..\..\..\..\config_env.bat"
 
 set "PYTHONUNBUFFERED=1"
 
+REM snmpsim needs HOMEPATH set and it seems to be missing when run in jenkins on some nodes
+if "%HOMEPATH%" == "" (
+    for %%F in ("%USERPROFILE%") do set HOMEPATH=%%~pnF
+)
+
 REM Command line arguments always passed to the test script
 SET ARGS=--test_and_emulator %~dp0
 "%PYTHON3%" -u "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" %ARGS% %*
