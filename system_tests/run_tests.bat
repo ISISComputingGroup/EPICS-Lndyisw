02699ef0b17936b36a656b60143c983a20da6f69
Lowri Jenkins <lowri.jenkins@stfc.ac.uk>
2024-05-13 11:50:04 +0100

    system_tests/run_tests.bat

--- system_tests/run_tests.bat
+++ system_tests/run_tests.bat
@@ -11,9 +11,4 @@ set "PYTHONUNBUFFERED=1"
 REM Command line arguments always passed to the test script
 SET ARGS=--test_and_emulator %~dp0
 "%PYTHON3%" -u "%EPICS_KIT_ROOT%\support\IocTestFramework\master\run_tests.py" %ARGS% %*
-set errcode=!errorlevel!
-
-REM errcode preserves errorlevel so not overwritten by taskkill
-taskkill /IM snmpsim-command-responder.exe /F >NUL 2>&1
-
-EXIT /b !errcode!
+IF %ERRORLEVEL% NEQ 0 EXIT /b %errorlevel%
