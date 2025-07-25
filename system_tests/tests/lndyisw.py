d02570a8dd27812c2c9a82718b0f1646b6674b27
Lowri Jenkins <lowri.jenkins@stfc.ac.uk>
2025-07-25 15:58:47 +0100

    fix formatting again

--- system_tests/tests/lndyisw.py
+++ system_tests/tests/lndyisw.py
@@ -2,10 +2,10 @@ import unittest
 
 from parameterized import parameterized
 from utils.channel_access import ChannelAccess  # type: ignore
-from utils.emulator_launcher import CommandLineEmulatorLauncher # type: ignore
-from utils.ioc_launcher import get_default_ioc_dir # type: ignore
-from utils.test_modes import TestModes # type: ignore
-from utils.testing import get_running_lewis_and_ioc # type: ignore
+from utils.emulator_launcher import CommandLineEmulatorLauncher  # type: ignore
+from utils.ioc_launcher import get_default_ioc_dir  # type: ignore
+from utils.test_modes import TestModes  # type: ignore
+from utils.testing import get_running_lewis_and_ioc  # type: ignore
 
 DEVICE_PREFIX = "LNDYISW_01"
 
