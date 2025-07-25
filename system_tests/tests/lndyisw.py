b6882fcd8a800cce556bc0999f414f63325dc62a
Lowri Jenkins <lowri.jenkins@stfc.ac.uk>
2025-07-25 15:41:06 +0100

    ruff format

--- system_tests/tests/lndyisw.py
+++ system_tests/tests/lndyisw.py
@@ -73,44 +73,59 @@ class LndyiswTests(unittest.TestCase):
         self.ca.assert_that_pv_is_not("LOCATION", new_value)
         self.ca.assert_that_pv_is("LOCATION", old_value)
 
-    @parameterized.expand([("_odds", "1,0,1,0,1,0,1,0"),("_even", "0,1,0,1,0,1,0,1"),("_all", "1,1,1,1,1,1,1,1")
-    ,("_first_half", "1,1,1,1,0,0,0,0"),("_second_half", "0,0,0,0,1,1,1,1")])
+    @parameterized.expand(
+        [
+            ("_odds", "1,0,1,0,1,0,1,0"),
+            ("_even", "0,1,0,1,0,1,0,1"),
+            ("_all", "1,1,1,1,1,1,1,1"),
+            ("_first_half", "1,1,1,1,0,0,0,0"),
+            ("_second_half", "0,0,0,0,1,1,1,1"),
+        ]
+    )
     def test_status_split_properly(self, _, vals):
         self.ca.set_pv_value("STATUS:ALLSET", "0,0,0,0,0,0,0,0")
         self.ca.set_pv_value("STATUS:ALLSET", vals)
-        check_outlet_status_calc(vals.split(","),self.ca)
-        check_outlet_status_bi(vals.split(","),self.ca)
+        check_outlet_status_calc(vals.split(","), self.ca)
+        check_outlet_status_bi(vals.split(","), self.ca)
 
-    @parameterized.expand([("_index_1", 10, "AA"),("_index_2", 20, "BB"),("_index_3", 30, "CC"),
-    ("_index_4", 40, "DD"),("_index_5", 50, "EE"),("_index_6", 60, "FF"),("_index_7", 70, "GG"),
-    ("_index_8", 80, "HH")])
+    @parameterized.expand(
+        [
+            ("_index_1", 10, "AA"),
+            ("_index_2", 20, "BB"),
+            ("_index_3", 30, "CC"),
+            ("_index_4", 40, "DD"),
+            ("_index_5", 50, "EE"),
+            ("_index_6", 60, "FF"),
+            ("_index_7", 70, "GG"),
+            ("_index_8", 80, "HH"),
+        ]
+    )
     def test_WHEN_curr_val_i_updates_THEN_correct_allset_update(self, _, index, curr):
-        allset = ["0","0","0","0","0","0","0","0"]
+        allset = ["0", "0", "0", "0", "0", "0", "0", "0"]
         self.ca.set_pv_value("STATUS:ALLSET", ",".join(allset))
         self.ca.assert_that_pv_is(f"STATUS:CURRVAL.{curr}", "0")
-        self.ca.set_pv_value("STATUS:CURRVAL.I", index+1)
+        self.ca.set_pv_value("STATUS:CURRVAL.I", index + 1)
         self.ca.assert_that_pv_is(f"STATUS:CURRVAL.{curr}", "1")
-        allset[int(index/10)-1]="1"
+        allset[int(index / 10) - 1] = "1"
         self.ca.assert_that_pv_is("STATUS:ALLSET", ",".join(allset))
         self.ca.set_pv_value("STATUS:CURRVAL.I", index)
         self.ca.assert_that_pv_is(f"STATUS:CURRVAL.{curr}", "0")
-        
-
 
     def test_LNDYISW_ioc_WHEN_two_sets_THEN_both_work(self):
         self.ca.set_pv_value("STATUS:ALLSET", "0,0,0,0,0,0,0,0")
-        
+
         self.ca.set_pv_value("OUTLET1:STATUS:SP", 1, sleep_after_set=0)
         self.ca.set_pv_value("OUTLET2:STATUS:SP", 1)
-        
+
         self.ca.assert_that_pv_is("OUTLET1:STATUS", "ON")
         self.ca.assert_that_pv_is("OUTLET2:STATUS", "ON")
 
         self.ca.set_pv_value("OUTLET1:STATUS:SP", 0, sleep_after_set=0)
         self.ca.set_pv_value("OUTLET2:STATUS:SP", 0)
-        
+
         self.ca.assert_that_pv_is("OUTLET1:STATUS", "OFF")
         self.ca.assert_that_pv_is("OUTLET2:STATUS", "OFF")
+
 
 def check_outlet_status_calc(vals, ca):
     ca.assert_that_pv_is("OUTLET:STATUS1.B", float(vals[0]))
@@ -123,6 +138,7 @@ def check_outlet_status_calc(vals, ca):
     ca.assert_that_pv_is("OUTLET:STATUS2.H", float(vals[6]))
     ca.assert_that_pv_is("OUTLET:STATUS2.I", float(vals[7]))
 
+
 def check_outlet_status_bi(vals, ca):
     status = ["OFF", "ON"]
     ca.assert_that_pv_is("OUTLET1:STATUS", status[int(vals[0])])
@@ -134,4 +150,3 @@ def check_outlet_status_bi(vals, ca):
     ca.assert_that_pv_is("OUTLET6:STATUS", status[int(vals[5])])
     ca.assert_that_pv_is("OUTLET7:STATUS", status[int(vals[6])])
     ca.assert_that_pv_is("OUTLET8:STATUS", status[int(vals[7])])
-    
